ebe9ec560b42581cc5e246f191ea2fefcfe755ea
JulioSouza09 <juliosouza.ux@gmail.com>
2025-09-17 17:54:06 +0100

    Implemented a Makefile, added philo.h and main.c

---
+++ philo/include/philo.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jcesar-s <jcesar-s@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/17 17:43:29 by jcesar-s          #+#    #+#             */
+/*   Updated: 2025/09/17 17:46:50 by jcesar-s         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+# define TRUE 1
+# define FALSE 0
+# define TEST "this is a prove that including philo.h is working.\n"
+
+# include <stdio.h>
+#endif
